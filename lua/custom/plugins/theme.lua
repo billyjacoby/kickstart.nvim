451dbee224c0a183cda6ec400ba6dfebfbf68b22
billyjacoby <billyjacoby@gmail.com>
2025-01-10 16:48:57 -0500

    lsp updates

---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,14 @@
+-- return {
+--     "nyoom-engineering/oxocarbon.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.opt.background = "dark"
+--         vim.cmd.colorscheme("oxocarbon")
+--     end,
+-- }
+return {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+}
